27950d3ab1cfcc4184b336ccc17517bab9613d65
Mozzoer <chayakorn.siri@hotmail.com>
2024-11-10 23:48:07 +0700

    conf: Create react app edit: Moving 'Dockerfile' to Server folder

--- Server/socket/socket-front/src/index.js
+++ Server/socket/socket-front/src/index.js
@@ -1,13 +1,17 @@
-const express = require('express');
-const { createServer } = require('node:http');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const app = express();
-const server = createServer(app);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
-
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
-});
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
