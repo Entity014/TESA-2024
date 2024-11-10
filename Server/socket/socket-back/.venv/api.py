61485f1801d4a2d70200c774cd5041b71f34d8aa
Mozzoer <chayakorn.siri@hotmail.com>
2024-11-10 17:45:48 +0700

    edit: Managing folder structure

--- Server/socket/socket-back/.venv/api.py
+++ Server/socket/socket-back/.venv/api.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "<p>Hello, World!</p>"
+
+@app.route("/data",methods=["GET"])
+def get_data():
+    data={"key1":"value1" , "key2":"value2", "key3":"value3"}
+    return jsonify(data),200
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=4000,debug=True)
