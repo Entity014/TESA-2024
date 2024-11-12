9ac72b75878d1f1a5e0301c22615c80ee8f0e568
mannaja <manman7852078520@gmail.com>
2024-11-12 10:13:44 +0700

    add hello.c

--- Hardware/day1/day 1 emb Linux/workshop/ex_02/hello.c
+++ Hardware/day1/day 1 emb Linux/workshop/ex_02/hello.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main(/* TBD */) {
-    if (/* TBD */) {
-        printf("Sawasdee %s\n", /* TBD */);
+int main(int argc, char *argv[]) {
+    if (argc == 2) {
+        printf("Sawasdee %s\n", argv[1]);
         char surname[20];
         printf("Enter your surname: ");
-        if (fgets(surname, sizeof(surname), /* TBD */) != NULL) {
-            printf("Hello %s %s", /* TBD */, surname);
+        if (fgets(surname, sizeof(surname), stdin) != NULL) {
+            printf("Hello %s %s\n", argv[1], surname);
         }
     } else {
-        printf("Use ./hello YOUR_NAME\n");
+        printf("Use hello YOUR_NAME\n");
     }
 }
