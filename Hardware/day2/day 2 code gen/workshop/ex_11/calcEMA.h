11f1cd50fee76b76f3807a3114cb9b728c933206
mannaja <manman7852078520@gmail.com>
2024-11-13 10:56:10 +0700

    s

---
+++ Hardware/day2/day 2 code gen/workshop/ex_11/calcEMA.h
@@ -0,0 +1,31 @@
+/*
+ * Trial License - for use to evaluate programs for possible purchase as
+ * an end-user only.
+ *
+ * calcEMA.h
+ *
+ * Code generation for function 'calcEMA'
+ *
+ */
+
+#ifndef CALCEMA_H
+#define CALCEMA_H
+
+/* Include files */
+#include "rtwtypes.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Function Declarations */
+extern void calcEMA(const double data[100], unsigned int N, double ema[100]);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+/* End of code generation (calcEMA.h) */
---
+++ Hardware/day2/day 2 code gen/workshop/ex_11/calcEMA_terminate.h
@@ -0,0 +1,31 @@
+/*
+ * Trial License - for use to evaluate programs for possible purchase as
+ * an end-user only.
+ *
+ * calcEMA_terminate.h
+ *
+ * Code generation for function 'calcEMA_terminate'
+ *
+ */
+
+#ifndef CALCEMA_TERMINATE_H
+#define CALCEMA_TERMINATE_H
+
+/* Include files */
+#include "rtwtypes.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Function Declarations */
+extern void calcEMA_terminate(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+/* End of code generation (calcEMA_terminate.h) */
